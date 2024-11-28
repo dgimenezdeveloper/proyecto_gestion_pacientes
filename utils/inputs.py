a4191fc8e4776c77044ad86d9fa730cc5c2e0758
Daro <dgimenez.developer@gmail.com>
2024-11-28 09:38:23 -0300

    Se refactorizan funciones para validacion

--- utils/inputs.py
+++ utils/inputs.py
@@ -5,21 +5,31 @@ from datetime import datetime
 def solicitar_dni():
     while True:
         try:
-            return int(input("Ingrese DNI: "))
+            dni = input("Ingrese DNI: ").strip()
+            if not dni:
+                print("Error: El DNI no puede estar vacío.")
+                continue
+            return int(dni)
         except ValueError:
             print("Error: El DNI debe ser un número entero.")
 
 def solicitar_nombre():
-    return input("Nombre: ")
+    while True:
+        nombre = input("Nombre: ").strip()
+        if not nombre:
+            print("Error: El nombre no puede estar vacío.")
+        else:
+            return nombre
 
 def solicitar_fecha_nacimiento():
     while True:
-        fecha_nac = input("Fecha de Nacimiento (YYYY-MM-DD): ")
+        fecha_nac = input("Fecha de Nacimiento (DD-MM-YYYY): ")
         try:
-            datetime.strptime(fecha_nac, "%Y-%m-%d")
-            return fecha_nac
+            # Convertir de DD-MM-YYYY a YYYY-MM-DD
+            fecha_nac_formateada = datetime.strptime(fecha_nac, "%d-%m-%Y").strftime("%Y-%m-%d")
+            return fecha_nac_formateada
         except ValueError:
-            print("Error: La fecha de nacimiento debe estar en el formato YYYY-MM-DD.")
+            print("Error: La fecha de nacimiento debe estar en el formato DD-MM-YYYY.")
 
 def solicitar_lista(mensaje):
     return input(mensaje).split(",")
