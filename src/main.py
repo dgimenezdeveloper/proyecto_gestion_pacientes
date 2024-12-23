0c266a14167e1640c3f27f890b5d05ffc49fa0c1
Daro <dgimenez.developer@gmail.com>
2024-12-23 11:06:50 -0300

    se mejora importacion

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-# src/main.py
-
 import sys
 import os
 
-# Agrega el directorio raíz del proyecto a la ruta de búsqueda de módulos
+# Agregar el directorio raíz del proyecto al PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from menu import menu_principal
